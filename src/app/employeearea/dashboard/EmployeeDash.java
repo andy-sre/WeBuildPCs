736dab55fffe4acaaf1e0b4cd30223545c5130c6
IAmAndy <c17475384@mytudublin.ie>
2021-04-21 10:28:04 +0100

    C17475384
    package renaming

---
+++ src/app/employeearea/dashboard/EmployeeDash.java
@@ -0,0 +1,58 @@
+package app.employeearea.dashboard;
+
+import app.App;
+import app.employeearea.dashboard.orders.ViewOrders;
+import app.employeearea.dashboard.profiledash.ProfileDash;
+import app.employeearea.dashboard.stock.StockDash;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EmployeeDash extends JFrame{
+    private JPanel panel;
+    private JButton logoutButton;
+    private JButton stockButton;
+    private JButton viewOrdersButton;
+    private JButton editDetailsButton;
+    private JLabel welcomeLabel;
+
+    public EmployeeDash(int employeeID, String fname) {
+        welcomeLabel.setText("Welcome, " + fname + " to your dashboard");
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setTitle("Computer Shop - Welcome");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(0,0,size.width, size.height);
+        this.setVisible(true);
+        this.add(panel);
+        stockButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new StockDash(employeeID, fname);
+                dispose();
+            }
+        });
+        editDetailsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ProfileDash(employeeID, fname);
+                dispose();
+            }
+        });
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new App();
+                dispose();
+            }
+        });
+        viewOrdersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ViewOrders(employeeID, fname);
+                dispose();
+            }
+        });
+    }
+}
