bd76ef12d54c6582002edaee9c9518c3ed3520ed
IAmAndy <c17475384@mytudublin.ie>
2021-04-26 01:33:04 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import app.App;
 import javax.swing.*;
 
 public class Main {
+    //AS & AH
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new App());
     }
